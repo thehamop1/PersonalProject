ce86121d86f909ec7b99270c60c95686aae41a59
cristian gutierrez <cristian.gutierrez.56@my.csun.edu>
2023-08-12 17:23:14 -0700

    Adding a hang detector

--- src/main.cpp
+++ src/main.cpp
@@ -3,18 +3,5 @@
 
 int main()
 {
-  int num{0};
 
-  Queue<int> queue;
-
-  queue.push(1);
-  queue.push(2);
-  queue.push(3);
-
-  while(!queue.empty()){
-    queue.pop(num);
-    std::cout << "The Num: " << num << std::endl;
-  }; 
-
-  return 0;
 }
